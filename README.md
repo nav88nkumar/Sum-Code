88dae34f1c3d7c6c56af2665b4b83f3127d04622
Naveen <nav88nkumar@gmail.com>
2021-01-18 13:13:55 +0530

    added the second line

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Sum-Code
 Going to create a sum code using python
+
+Adding a sum= a+b
--- Sum.py
+++ Sum.py
@@ -2,3 +2,4 @@ a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
 Sum= a+b
 
+
